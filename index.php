00eb5373db0498872ef291f5435f612002ddb08d
LoveshGurung <loveshgurung1@gmail.com>
2024-12-05 23:46:01 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Happy Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@100;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="css/common.css">
-    <style>
-    .availability-form{
-      margin-top: -50px;
-      z-index: 11;
-      position: relative;
-    }
-    @media screen and (max-width: 57){
-      .availability-form{
-      margin-top: 25px;
-      padding: 0 35px;
-      }
 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Happy Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="css/common.css">
+  
+<style>
+  .availability-form{
+    margin-top: -50px;
+    z-index: 11;
+    position: relative;
+  }
+  @media screen and (max-width: 57){
+    .availability-form{
+    margin-top: 25px;
+    padding: 0 35px;
     }
+
+  }
 
     
 </Style>
@@ -29,43 +31,43 @@
 </head>
 
 <body class="bg-light">
-    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top ">
-    <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">HAPPY HOME</a>
-        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link me-3" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-3" href="#">Rooms</a>
-            </li>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link me-3" href="#">Facilities</a>
-            </li>            </li>
-            <li class="nav-item">
-            <a class="nav-link me-3" href="#">Contact Us</a>
-            </li>            </li>
-            <li class="nav-item">
-            <a class="nav-link me-3" href="#">about</a>
-            </li>           
-          </ul>
-          <div class="d-flex">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-             Login
-            </button>
-            <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
-             Register
-            </button>
-          </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top ">
+  <div class="container-fluid">
+      <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">HAPPY HOME</a>
+      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+          <a class="nav-link me-3" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link me-3" href="#">Rooms</a>
+          </li>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link me-3" href="#">Facilities</a>
+          </li>            </li>
+          <li class="nav-item">
+          <a class="nav-link me-3" href="#">Contact Us</a>
+          </li>            </li>
+          <li class="nav-item">
+          <a class="nav-link me-3" href="#">about</a>
+          </li>           
+        </ul>
+        <div class="d-flex">
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+            Login
+          </button>
+          <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
+            Register
+          </button>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 
     <!-- Modal -->
 <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -163,7 +165,7 @@
 
 <!--carousel-->
 
-  <div class="container-fluid px-lg-4 mt-4">
+<div class="container-fluid px-lg-4 mt-4">
     <div class="swiper swiper-container">
       <div class="swiper-wrapper">
         <div class="swiper-slide">
@@ -180,7 +182,7 @@
         </div>
       </div>
     </div>
-  </div>
+</div>
 
 
 <!--check availability -->
