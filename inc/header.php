2882fddfb4c661ead4c62ac8807781dd41029dcf
LoveshGurung <loveshgurung1@gmail.com>
2024-12-27 23:45:02 +0545

    user creds

--- inc/header.php
+++ inc/header.php
@@ -67,65 +67,65 @@
 </div>
 
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-<div class="modal-dialog modal-lg">
-    <div class="modal-content">
-    <form>
-        <div class="modal-header">
-            <h5 class="modal-title d-flex align-items-center">
-                <i class="bi bi-person-lines-fill fs-3 me-2"></i> User Registration
-            </h5>
-            <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-            Note: Your details must match with your ID(citezenship, passport, driving license, etc.) 
-            that will be required during check-in.
-            </span>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Name</label>
-                        <input type="text" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Email</label>
-                        <input type="email" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Phone Number</label>
-                        <input type="number" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Picture</label>
-                        <input type="file" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-12 ps-0 mb-3">
-                        <label class="form-label">Address</label>
-                        <input type="text" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Postal code</label>
-                        <input type="number" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Date of birth</label>
-                        <input type="date" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none" >
-                    </div>
-                    <div class="col-md-6 ps-0 mb-3">
-                        <label class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control shadow-none" >
-                    </div>
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <form id="register-form">
+                <div class="modal-header">
+                    <h5 class="modal-title d-flex align-items-center">
+                        <i class="bi bi-person-lines-fill fs-3 me-2"></i> User Registration
+                    </h5>
+                    <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-            </div> 
-            <div class="text-center my-1">
-            <button type="submit" class="btn btn-dark shadow-none">Register</button>
-            </div>     
+                <div class="modal-body">
+                    <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                    Note: Your details must match with your ID(citezenship, passport, driving license, etc.) 
+                    that will be required during check-in.
+                    </span>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">Name</label>
+                                <input name="name" type="text" class="form-control shadow-none" required >
+                            </div>
+                            <div class="col-md-6 p-0 mb-3">
+                                <label class="form-label">Email</label>
+                                <input name="email" type="email" class="form-control shadow-none" required>
+                            </div>
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">Phone Number</label>
+                                <input name="phonenum" type="number" class="form-control shadow-none" required >
+                            </div>
+                            <div class="col-md-6 p-0 mb-3">
+                                <label class="form-label">Picture</label>
+                                <input name="profile" type="file" accept=".jpg, .jpeg, .png, .webp" class="form-control shadow-none" required>
+                            </div>
+                            <div class="col-md-12 ps-0 mb-3">
+                                <label class="form-label">Address</label>
+                                <textarea name="address" type="text" class="form-control shadow-none" rows="1" required></textarea>
+                            </div>
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">P code</label>
+                                <input type="number" class="form-control shadow-none" required>
+                            </div>
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">Date of birth</label>
+                                <input type="date" class="form-control shadow-none" required >
+                            </div>
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">Password</label>
+                                <input type="password" class="form-control shadow-none"required >
+                            </div>
+                            <div class="col-md-6 ps-0 mb-3">
+                                <label class="form-label">Confirm Password</label>
+                                <input type="password" class="form-control shadow-none" required>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="text-center my-1">
+                    <button type="submit" class="btn btn-dark shadow-none">Register</button>
+                    </div>     
+                </div>
+            </form>
         </div>
-        </form>
     </div>
-</div>
 </div>
